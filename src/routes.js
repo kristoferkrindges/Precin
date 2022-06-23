208ab07e12495aac57fd3b7d2ddf235e931215cd
dionardomarques <dionardogianluca@gmail.com>
2022-06-22 23:22:33 -0300

    search_result_in_progress

--- src/routes.js
+++ src/routes.js
@@ -1,22 +1,28 @@
 import React from "react";
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomeScreen from "./Pages/Home";
-import About from './Pages/About';
-import Profile from './Pages/Profile';
-import Login from './Pages/Login';
-import ErrorPage from './Pages/ErrorPage';
+import About from "./Pages/About";
+import Profile from "./Pages/Profile";
+import Login from "./Pages/Login";
+import SearchResultScreen from "./Pages/SearchResult";
+import ErrorPage from "./Pages/ErrorPage";
 
-export default function Routess(){
-    return(
-        <Router>
-            <Routes>
-                <Route path="/" element={<HomeScreen/>}/>
-                <Route path="/login" element={<Login />} />
-                <Route path="/about" element={<About />} />
-                <Route path="/profile" element={<Profile />} />
-                <Route path="/profile/:username" element={<Profile />} />
-                <Route path="*" element={<ErrorPage />} />
-            </Routes>
-        </Router>
-    )
+export default function Routess() {
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<HomeScreen />} />
+				<Route path="/login" element={<Login />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/profile" element={<Profile />} />
+				<Route path="/profile/:username" element={<Profile />} />
+				<Route path="/search_result" element={<SearchResultScreen />} />
+				<Route
+					path="/search_result/:product"
+					element={<SearchResultScreen />}
+				/>
+				<Route path="*" element={<ErrorPage />} />
+			</Routes>
+		</Router>
+	);
 }
